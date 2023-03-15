17d2a8c590bea851be2432a9d8bf4241d66120ea
Suman Kisku <sumankisku1@gmail.com>
2023-03-16 00:57:36 +0530

    test

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,12 @@ const App = () => {
   return (
     <div id="main">
                {/* Do not remove the main div */}
+               <ol key="relativeList">
+                <li key="relativeListItem1">Mama</li>
+                <li key="relativeListItem2">Pisima</li>
+                <li key="relativeListItem3">Masi</li>
+                <li key="relativeListItem4">Jamai Babu</li>
+               </ol>
     </div>
   )
 }
